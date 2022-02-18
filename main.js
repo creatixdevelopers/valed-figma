2cafa2cdafa149d39660b1a752477b058ac3a249
Chirag Chopra <iamchiragchopra@gmail.com>
2022-02-18 14:47:02 +0530

    free session form incomplete

--- main.js
+++ main.js
@@ -143,7 +143,7 @@
         loop: true,
         speed: 1000,
         autoplay: {
-            delay: 3000,
+            delay: 30000,
         },
         effect: 'coverflow',
         grabCursor: true,
@@ -155,6 +155,10 @@
             depth: 300,
             modifier: 1,
             slideShadows: true,
+        },
+        pagination: {
+            el: '#programDots',
+            clickable: true,
         },
         navigation: {
             nextEl: '.swiper-button-next',
@@ -205,4 +209,40 @@
             prevEl: '.swiper-button-prev',
         },
     });
+
+    mdtimepicker.defaults({theme: 'green', hourPadding: true, clearBtn: true});
+
+    function log(message) {
+        document.querySelector('#loggerTxt').value = message
+    }
+
+    window.onload = function () {
+        mdtimepicker('#timePicker', {
+            events: {
+                timeChanged: function (data) {
+                    console.log('timeChanged', data)
+                    log('timeChanged: ' + data.value)
+                }
+            }
+        })
+
+        mdtimepicker('#picker2', {
+            readOnly: false, is24hour: true, //format: 'h:mm tt',
+            events: {
+                ready: function () {
+                    console.log('ready', this)
+                },
+                shown: function () {
+                    console.log('shown', this)
+                },
+                hidden: function () {
+                    console.log('hidden', this)
+                },
+                timeChanged: function (data) {
+                    console.log('timeChanged', data)
+                    log('timeChanged: ' + data.value)
+                }
+            }
+        })
+    }
 })()
